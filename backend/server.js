604fce8509112eb85102e0d0625faa3a12a42652
Shelesh Pratap Singh <s.p.singh.1603@gmail.com>
2025-07-20 15:36:51 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // import cors from "cors";
 
@@ -12,12 +13,14 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
+
 dotenv.config();
 
 
-// const __dirname = path.resolve();
+
 // const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 
 // app.use(cors())
@@ -28,11 +31,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes );
 
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
